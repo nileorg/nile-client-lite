5c9723c1f69181afd1375ff4966cb51b5e7fbf3e
marcocastignoli <marco.castignoli@gmail.com>
2020-04-28 21:45:54 +0200

    set website title "Nile"

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ module.exports = {
     config.module.rule('eslint').use('eslint-loader').options({
       fix: true
     })
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Nile'
+        return args
+      })
   },
   pluginOptions: {
     i18n: {
