1e5e002ee2ef58831cbbcb792a6bbc952b3f0f29
Egidio Caprino <egidio.caprino@gmail.com>
2020-09-04 18:13:55 +0200

    feat(about): translate about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,43 +10,35 @@
     <el-main>
       <el-row>
         <el-col>
-          <p>Nile is a decentralized, forever free for everyone, local ecommerce platform.</p>
+          <p>{{ $t('aboutDescription') }}</p>
         </el-col>
       </el-row>
       <el-row :gutter="12">
         <el-col :span="8">
           <el-card shadow="hover">
             <div slot="header" class="clearfix">
-              <h2>Free</h2>
+              <h2>{{ $t('aboutFree') }}</h2>
             </div>
             <el-image src="https://via.placeholder.com/600" :fit="fit" />
-            <p>
-              Nile is completely free. No commission for the shop, nor for the customer. Forefer.
-            </p>
+            <p>{{ $t('aboutFreeDescription') }}</p>
           </el-card>
         </el-col>
         <el-col :span="8">
           <el-card shadow="hover">
             <div slot="header" class="clearfix">
-              <h2>Decentralized</h2>
+              <h2>{{ $t('aboutDecentralized') }}</h2>
             </div>
             <el-image src="https://via.placeholder.com/600" :fit="fit" />
-            <p>
-              Nile doesn't run on a server. It runs on a network controlled by its community.
-              The shops directly connect to Nile network for selling products.
-            </p>
+            <p>{{ $t('aboutDecentralizedDescription') }}</p>
           </el-card>
         </el-col>
         <el-col :span="8">
           <el-card shadow="hover">
             <div slot="header" class="clearfix">
-              <h2>Local</h2>
+              <h2>{{ $t('aboutLocal') }}</h2>
             </div>
             <el-image src="https://via.placeholder.com/600" :fit="fit" />
-            <p>
-              Search for shops in your city.
-              Collect your order directly in the store or have it delivered to your place.
-            </p>
+            <p>{{ $t('aboutLocalDescription') }}</p>
           </el-card>
         </el-col>
       </el-row>
